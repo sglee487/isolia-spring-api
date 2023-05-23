f609a2794cd4c34c3066c281f09850ab08a331f4
sglee <sglee487@gmail.com>
2023-05-23 18:09:50 +0900

    adding BoardGetResponse

--- src/main/kotlin/com/group/isolia_api/schemas/board/response/BoardResponse.kt
+++ src/main/kotlin/com/group/isolia_api/schemas/board/response/BoardResponse.kt
@@ -1,4 +1,11 @@
 package com.group.isolia_api.schemas.board.response
 
-class BoardCreateResponse {
+import com.group.isolia_api.domain.Board
+import com.group.isolia_api.domain.User
+
+class BoardGetResponse(
+    val borad: Board,
+    val user: User
+) {
+
 }

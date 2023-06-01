e3f12f65453b12ca252dd1faf3d09c6786c32b61
sglee <sglee487@gmail.com>
2023-06-01 18:06:39 +0900

    making sw socket

--- src/main/kotlin/com/group/isolia_api/controller/MineSweeperController.kt
+++ src/main/kotlin/com/group/isolia_api/controller/MineSweeperController.kt
@@ -1,9 +1,19 @@
 package com.group.isolia_api.controller
 
+import com.group.isolia_api.repository.minesweeper.MineSweeperRepository
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @CrossOrigin(origins = ["*"])
-class MineSweeperController {
+class MineSweeperController(
+    val mineSweeperRepository: MineSweeperRepository
+) {
+
+    @GetMapping("/reset")
+    fun reset() {
+        mineSweeperRepository.reset()
+    }
+
 }
--- src/main/kotlin/com/group/isolia_api/repository/minesweeper/MineSweeperRepository.kt
+++ src/main/kotlin/com/group/isolia_api/repository/minesweeper/MineSweeperRepository.kt
@@ -1,5 +1,8 @@
 package com.group.isolia_api.repository.minesweeper
 
+import org.springframework.stereotype.Repository
+
+@Repository
 class MineSweeperRepository {
     fun reset() {
         MineFieldMemoryDatabase.reset()

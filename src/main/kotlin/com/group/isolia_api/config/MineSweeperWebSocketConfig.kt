30a15219e6e56849c918865893228be9d3a3172b
sglee <sglee487@gmail.com>
2023-06-02 00:25:35 +0900

    making minesweeper ws

--- src/main/kotlin/com/group/isolia_api/config/MineSweeperWebSocketConfig.kt
+++ src/main/kotlin/com/group/isolia_api/config/MineSweeperWebSocketConfig.kt
@@ -2,20 +2,23 @@ package com.group.isolia_api.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
 @EnableWebSocketMessageBroker
+@CrossOrigin(origins = ["http://localhost:5173"])
 class MineSweeperWebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry.enableSimpleBroker("/subscribe")
         registry.setApplicationDestinationPrefixes("/publish")
+        registry.setUserDestinationPrefix("/user")
     }
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/ws-connect").setAllowedOrigins("*").withSockJS()
+        registry.addEndpoint("/ws-connect").setAllowedOriginPatterns("*").withSockJS()
     }
 }
--- src/main/kotlin/com/group/isolia_api/controller/MineSweeperController.kt
+++ src/main/kotlin/com/group/isolia_api/controller/MineSweeperController.kt
@@ -1,6 +1,9 @@
 package com.group.isolia_api.controller
 
 import com.group.isolia_api.repository.minesweeper.MineSweeperRepository
+import org.springframework.messaging.handler.annotation.Header
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,12 +11,24 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @CrossOrigin(origins = ["*"])
 class MineSweeperController(
-    val mineSweeperRepository: MineSweeperRepository
+    val mineSweeperRepository: MineSweeperRepository,
+    val simpleMessagingTemplate: SimpMessagingTemplate
 ) {
 
     @GetMapping("/reset")
     fun reset() {
         mineSweeperRepository.reset()
+    }
+
+    @MessageMapping("/hello")
+    fun greeting(message: String){
+        simpleMessagingTemplate.convertAndSend("/subscribe/mine", message)
+    }
+
+    @MessageMapping("/join")
+    fun generateNickname(@Header("simpSessionId") sessionId: String) {
+        println(sessionId)
+        simpleMessagingTemplate.convertAndSendToUser(sessionId, "/subscribe/mine", "jourge")
     }
 
 }

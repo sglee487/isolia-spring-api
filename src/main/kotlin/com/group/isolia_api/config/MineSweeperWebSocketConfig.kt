0e9193745084f66a703b41120db1001c2cb03167
sglee <sglee487@gmail.com>
2023-06-05 00:58:16 +0900

    making minesweeper ws

--- src/main/kotlin/com/group/isolia_api/config/MineSweeperWebSocketConfig.kt
+++ src/main/kotlin/com/group/isolia_api/config/MineSweeperWebSocketConfig.kt
@@ -1,11 +1,16 @@
 package com.group.isolia_api.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.server.ServerHttpRequest
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.socket.WebSocketHandler
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler
+import java.security.Principal
+import java.util.*
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -15,10 +20,26 @@ class MineSweeperWebSocketConfig : WebSocketMessageBrokerConfigurer {
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry.enableSimpleBroker("/subscribe")
         registry.setApplicationDestinationPrefixes("/publish")
-        registry.setUserDestinationPrefix("/user")
+        registry.setUserDestinationPrefix("/user") // 없어도 기본값으로 들어있다.
     }
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/ws-connect").setAllowedOriginPatterns("*").withSockJS()
+        registry.addEndpoint("/ws-connect").setAllowedOriginPatterns("*").setHandshakeHandler(CustomHandshakeHandler).withSockJS()
+    }
+}
+
+class StompPrincipal(private val name: String): Principal {
+    override fun getName(): String {
+        return name
+    }
+}
+
+object CustomHandshakeHandler: DefaultHandshakeHandler() {
+    override fun determineUser(
+        request: ServerHttpRequest,
+        wsHandler: WebSocketHandler,
+        attributes: MutableMap<String, Any>
+    ): Principal {
+        return StompPrincipal(UUID.randomUUID().toString())
     }
 }
--- src/main/kotlin/com/group/isolia_api/controller/MineSweeperController.kt
+++ src/main/kotlin/com/group/isolia_api/controller/MineSweeperController.kt
@@ -1,18 +1,23 @@
 package com.group.isolia_api.controller
 
 import com.group.isolia_api.repository.minesweeper.MineSweeperRepository
+import org.springframework.messaging.MessageHeaders
 import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor
+import org.springframework.messaging.simp.SimpMessageType
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.security.Principal
 
 @RestController
 @CrossOrigin(origins = ["*"])
 class MineSweeperController(
     val mineSweeperRepository: MineSweeperRepository,
-    val simpleMessagingTemplate: SimpMessagingTemplate
+    val simpleMessagingTemplate: SimpMessagingTemplate,
 ) {
 
     @GetMapping("/reset")
@@ -29,6 +34,74 @@ class MineSweeperController(
     fun generateNickname(@Header("simpSessionId") sessionId: String) {
         println(sessionId)
         simpleMessagingTemplate.convertAndSendToUser(sessionId, "/subscribe/mine", "jourge")
+    }
+
+//    @MessageMapping("/message")
+//    @SendToUser("/queue/reply")
+//    fun handleMessage(
+//        @Payload message: String,
+//        accessor: SimpMessageHeaderAccessor
+//    ): String {
+//        val sessionId = accessor.sessionId
+//        // Process the message and prepare the response
+//        val response = "Hello, $sessionId! You sent: $message"
+//        println(response)
+//        return response
+//    }
+
+    private fun createHeaders(sessionId: String?): MessageHeaders {
+        val headerAccessor = SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE)
+        sessionId?.let {
+            headerAccessor.sessionId = sessionId
+        }
+        headerAccessor.setLeaveMutable(true)
+        return headerAccessor.messageHeaders
+    }
+
+    @MessageMapping("/message")
+    fun handleMessage(
+        principal: Principal,
+        accessor: SimpMessageHeaderAccessor,
+        @Payload message: String,
+        @Header("simpSessionId") sessionId: String
+    ) {
+        println(principal)
+        println(principal.name)
+        println(message)
+        println(createHeaders(principal.name))
+        println(accessor.sessionId)
+        println(accessor.messageHeaders)
+        println(accessor.id)
+        println(sessionId)
+
+        simpleMessagingTemplate.convertAndSend("/subscribe/queue/reply", "dsfdsfdsf")
+        simpleMessagingTemplate.convertAndSend("/subscribe/${sessionId}/queue/reply", "dsfdsfdsf")
+        simpleMessagingTemplate.convertAndSend("/subscribe/user/${sessionId}/queue/reply", "dsfdsfdsf")
+
+        simpleMessagingTemplate.convertAndSend("/subscribe/${principal.name}/queue/reply", "dsfdsfdsf")
+        simpleMessagingTemplate.convertAndSend("/subscribe/user/${principal.name}/queue/reply", "dsfdsfdsf")
+
+        simpleMessagingTemplate.convertAndSendToUser(principal.name, "/queue/reply", "dsfdsfdsf")
+
+
+////        simpleMessagingTemplate.convertAndSendToUser()
+//        simpleMessagingTemplate.convertAndSendToUser(principal.name, "/queue/reply", "dsfdsfdsf")
+//        simpleMessagingTemplate.convertAndSendToUser(principal.name, "/queue/reply", "dsfdsfdsf", createHeaders(principal.name))
+//        simpleMessagingTemplate.convertAndSendToUser(accessor.sessionId!!, "/queue/reply", "dsfdsfdsf")
+//        simpleMessagingTemplate.convertAndSendToUser(accessor.sessionId!!, "/queue/reply", "dsfdsfdsf", createHeaders(accessor.sessionId!!))
+//
+//        simpleMessagingTemplate.convertAndSendToUser(principal.name, "/reply", "dsfdsfdsf")
+//        simpleMessagingTemplate.convertAndSendToUser(principal.name, "/reply", "dsfdsfdsf", createHeaders(principal.name))
+//        simpleMessagingTemplate.convertAndSendToUser(accessor.sessionId!!, "/reply", "dsfdsfdsf")
+//        simpleMessagingTemplate.convertAndSendToUser(accessor.sessionId!!, "/reply", "dsfdsfdsf", createHeaders(accessor.sessionId!!))
+
+
+
+        //        val sessionId = accessor.sessionId!!
+//        // Process the message and prepare the response
+//        val response = "Hello, $sessionId! You sent: $message"
+//        println(response)
+//        simpleMessagingTemplate.convertAndSendToUser(sessionId, "/queue/reply", response, accessor.messageHeaders)
     }
 
 }
